ddea63adeb71215937cc4d603df360219f07a438
Aidan Cornelius-Bell <ac@schemist.com.au>
2022-02-24 10:38:27 +1030

    Initial Commit

---
+++ PaperMetric/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  PaperMetric
+//
+//  Created by Aidan Cornelius-Bell on 24/2/2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ PaperMetric/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PaperMetric
+//
+//  Created by Aidan Cornelius-Bell on 24/2/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
